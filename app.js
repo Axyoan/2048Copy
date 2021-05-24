abf3148f03356ff1781214a62a5854efc3ae1705
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2021-05-23 23:29:38 -0500

    Kinda works, no animation, no game over

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const generateRandom = () => {
     return [rowElim, colElim, val];
 }
 
-const changeColor = (cell, val) => {
+const updateCell = (cell, val) => {
     cell.classList.remove(`val-0`)
     for (let i = 2; i <= 2048; i *= 2) {
         cell.classList.remove(`val-${i}`)
@@ -66,42 +66,161 @@ const initializeBoard = () => {
 
     frstCell.innerText = frstRet[2];
     sndCell.innerText = sndRet[2];
-    changeColor(frstCell, frstRet[2]);
-    changeColor(sndCell, sndRet[2]);
+    updateCell(frstCell, frstRet[2]);
+    updateCell(sndCell, sndRet[2]);
 }
 
 initializeBoard();
 
-const paintBoard = () => {
+const updateBoard = () => {
     for (let pos = 0; pos < 16; pos++) {
-        changeColor(cells[pos], board[Math.floor(pos / 4)][pos % 4]);
+        updateCell(cells[pos], board[Math.floor(pos / 4)][pos % 4]);
     }
+
+}
+
+
+
+const updateAvailable = () => {
+    available = [];
+    for (let row = 0; row < 4; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (board[row][col] == 0) {
+                available.push(row * 4 + col);
+            }
+        }
+    }
+}
+
+const gameOver = () => {
 
 }
 
 const keyUp = (e) => {
-    console.log("wtf");
     console.log("up");
-    for (let row = 1; row < 4; row++) {
-        for (let col = 0; col < 4; col++) {
-            board[0][col] += board[row][col];
+
+    let moved = false;
+    for (let col = 0; col < 4; col++) {
+        let lastIndex = 0;
+        for (let row = 1; row < 4; row++) {
+            if (board[row][col] !== 0 && (board[row][col] === board[lastIndex][col] || board[lastIndex][col] === 0)) {
+                // Merge cells or the top one is 0
+                board[lastIndex][col] += board[row][col];
+                console.log(`Merge row: ${row}, col: ${col}, lastIndex: ${lastIndex} cell: ${board[lastIndex][col]}`);
+                board[row][col] = 0;
+                moved = true;
+            }
+            else if (board[row][col] !== 0) {
+                //cell runs into an obstacle
+                console.log(`float row: ${row}, col: ${col}, lastIndex: ${lastIndex}`);
+                board[lastIndex + 1][col] = board[row][col];
+                if (lastIndex + 1 !== row) {
+                    board[row][col] = 0;
+                }
+                lastIndex++;
+            }
         }
     }
-    console.log(board);
-    paintBoard();
+    updateAvailable();
+    if (moved) {
+        generateRandom();
+    }
+    updateBoard();
 }
 
 const keyDown = (e) => {
     console.log("down");
+
+    let moved = false;
+    for (let col = 0; col < 4; col++) {
+        let lastIndex = 3;
+        for (let row = 2; row >= 0; row--) {
+            if (board[row][col] !== 0 && (board[row][col] === board[lastIndex][col] || board[lastIndex][col] === 0)) {
+                // Merge cells or the top one is 0
+                board[lastIndex][col] += board[row][col];
+                console.log(`Merge row: ${row}, col: ${col}, lastIndex: ${lastIndex} cell: ${board[lastIndex][col]}`);
+                board[row][col] = 0;
+                moved = true;
+            }
+            else if (board[row][col] !== 0) {
+                //cell runs into an obstacle
+                console.log(`float row: ${row}, col: ${col}, lastIndex: ${lastIndex}`);
+                board[lastIndex - 1][col] = board[row][col];
+                if (lastIndex - 1 !== row) {
+                    board[row][col] = 0;
+                }
+                lastIndex--;
+            }
+        }
+    }
+    updateAvailable();
+    if (moved) {
+        generateRandom();
+    }
+    updateBoard();
 }
 
 const keyLeft = (e) => {
-    console.log("left");
+    let moved = false;
+    for (let row = 0; row < 4; row++) {
+        let lastIndex = 0;
+        for (let col = 1; col < 4; col++) {
+            if (board[row][col] !== 0 && (board[row][col] === board[row][lastIndex] || board[row][lastIndex] === 0)) {
+                // Merge cells or the top one is 0
+                board[row][lastIndex] += board[row][col];
+                console.log(`Merge row: ${row}, col: ${col}, lastIndex: ${lastIndex} cell: ${board[row][lastIndex]}`);
+                board[row][col] = 0;
+                moved = true;
+            }
+            else if (board[row][col] !== 0) {
+                //cell runs into an obstacle
+                console.log(`float row: ${row}, col: ${col}, lastIndex: ${lastIndex}`);
+                board[row][lastIndex + 1] = board[row][col];
+                if (lastIndex + 1 !== col) {
+                    board[row][col] = 0;
+                }
+                lastIndex++;
+            }
+        }
+    }
+    updateAvailable();
+    if (moved) {
+        generateRandom();
+    }
+    updateBoard();
 }
 
 const keyRight = (e) => {
-    console.log("right");
+    let moved = false;
+    for (let row = 0; row < 4; row++) {
+        let lastIndex = 3;
+        for (let col = 2; col >= 0; col--) {
+            if (board[row][col] !== 0 && (board[row][col] === board[row][lastIndex] || board[row][lastIndex] === 0)) {
+                // Merge cells or the top one is 0
+                board[row][lastIndex] += board[row][col];
+                console.log(`Merge row: ${row}, col: ${col}, lastIndex: ${lastIndex} cell: ${board[row][lastIndex]}`);
+                board[row][col] = 0;
+                moved = true;
+            }
+            else if (board[row][col] !== 0) {
+                //cell runs into an obstacle
+                console.log(`float row: ${row}, col: ${col}, lastIndex: ${lastIndex}`);
+                board[row][lastIndex - 1] = board[row][col];
+                if (lastIndex - 1 !== col) {
+                    board[row][col] = 0;
+                }
+                lastIndex--;
+            }
+        }
+    }
+    updateAvailable();
+    if (moved) {
+        generateRandom();
+    }
+    updateBoard();
 }
+
+
 const keydown = (e) => {
     switch (e.code) {
         case "ArrowUp":
@@ -117,6 +236,9 @@ const keydown = (e) => {
             keyRight();
             return;
     }
+    if (available.length === 0) {
+        gameOver();
+    }
 }
 
 window.addEventListener('keydown', keydown);
@@ -125,3 +247,27 @@ const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", (e) => {
     initializeBoard();
 });
+
+
+
+let testBoard = () => {
+    /* board = [
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
+    ] */
+    board = [
+        [16, 4, 2, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+    board = [
+        [16, 4, 0, 0],
+        [4, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+    updateBoard();
+}
