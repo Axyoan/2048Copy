a6f07ec387393af55413b1d9f567c58a5e0ff259
Axyoan Marcelo Castillo <axyoan.marcelo@alumnos.udg.mx>
2021-05-24 00:16:28 -0500

    added mobile controls

--- app.js
+++ app.js
@@ -76,9 +76,7 @@ const updateBoard = () => {
     for (let pos = 0; pos < 16; pos++) {
         updateCell(cells[pos], board[Math.floor(pos / 4)][pos % 4]);
     }
-
 }
-
 
 
 const updateAvailable = () => {
@@ -243,20 +241,68 @@ const keydown = (e) => {
 
 window.addEventListener('keydown', keydown);
 
+const detectswipe = () => {
+    swipe_det = new Object();
+    swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    var min_x = 30;  //min x swipe for horizontal swipe
+    var max_x = 30;  //max x difference for vertical swipe
+    var min_y = 50;  //min y swipe for vertical swipe
+    var max_y = 60;  //max y difference for horizontal swipe
+    var direc = "";
+    window.addEventListener('touchstart', function (e) {
+        var t = e.touches[0];
+        swipe_det.sX = t.screenX;
+        swipe_det.sY = t.screenY;
+    }, false);
+    window.addEventListener('touchmove', function (e) {
+        e.preventDefault();
+        var t = e.touches[0];
+        swipe_det.eX = t.screenX;
+        swipe_det.eY = t.screenY;
+    }, false);
+    window.addEventListener('touchend', function (e) {
+        //horizontal detection
+        if ((((swipe_det.eX - min_x > swipe_det.sX) || (swipe_det.eX + min_x < swipe_det.sX)) && ((swipe_det.eY < swipe_det.sY + max_y) && (swipe_det.sY > swipe_det.eY - max_y) && (swipe_det.eX > 0)))) {
+            if (swipe_det.eX > swipe_det.sX) direc = "r";
+            else direc = "l";
+        }
+        //vertical detection
+        else if ((((swipe_det.eY - min_y > swipe_det.sY) || (swipe_det.eY + min_y < swipe_det.sY)) && ((swipe_det.eX < swipe_det.sX + max_x) && (swipe_det.sX > swipe_det.eX - max_x) && (swipe_det.eY > 0)))) {
+            if (swipe_det.eY > swipe_det.sY) direc = "d";
+            else direc = "u";
+        }
+        switch (direc) {
+            case "u":
+                keyUp();
+                break;
+            case "d":
+                keyDown();
+                break;
+            case "l":
+                keyLeft();
+                break;
+            case "r":
+                keyRight();
+                break;
+        }
+        if (available.length === 0) {
+            gameOver();
+        }
+        direc = "";
+        swipe_det.sX = 0; swipe_det.sY = 0; swipe_det.eX = 0; swipe_det.eY = 0;
+    }, false);
+}
+detectswipe();
+
 const resetBtn = document.querySelector("#resetBtn");
 resetBtn.addEventListener("click", (e) => {
     initializeBoard();
 });
 
-
+detectswipe();
 
 let testBoard = () => {
-    /* board = [
-        [2, 0, 0, 0],
-        [2, 0, 0, 0],
-        [2, 0, 0, 0],
-        [2, 0, 0, 0],
-    ] */
+
     board = [
         [16, 4, 2, 0],
         [0, 0, 0, 0],
@@ -268,6 +314,12 @@ let testBoard = () => {
         [4, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
+    ]
+    board = [
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
+        [2, 0, 0, 0],
     ]
     updateBoard();
 }
